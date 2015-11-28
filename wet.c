38815ee203b56ef94384058ef504020493d3fc0e
sameehj <sameeh.j@gmail.com>
2015-11-28 19:47:28 +0200

    photos-db: method implementations added
    Signed-off-by: sameehj <sameeh.j@gmail.com>

--- wet.c
+++ wet.c
@@ -1,10 +1,49 @@
 #include "wet.h"
+#include <libpq-fe.h>
 
-int main ( int arc, char **argv ) {
+PGconn *conn;
+int main(int arc, char **argv) {
 	char connect_param[200];
 	sprintf(connect_param,
-	"host=csl2.cs.technion.ac.il dbname=%s user=%s password=%s",
-	USERNAME, USERNAME, PASSWORD);
+			"host=csl2.cs.technion.ac.il dbname=%s user=%s password=%s",
+			USERNAME, USERNAME, PASSWORD);
 	PGconn *conn = PQconnectdb(connect_param);
-     return 0;
+	return 0;
+}
+
+void* addUser(const char* name) {
+
+}
+void* addUserMin(const char* name) {
+
+}
+void* removeUser(const char* id) {
+
+}
+void* addPhoto(const char* user_id, const char* photo_id) {
+
+}
+void* tagPhoto(const char* user_id, const char* photo_id, const char* info) {
+
+}
+void* photosTags() {
+
+}
+void* search(const char* word) {
+
+}
+void* commonTags(const char* k) {
+
+}
+void* mostCommonTags(const char* k) {
+
+}
+void* similarPhotos(const char* k, const char* j) {
+
+}
+void* autoPhotoOnTagOn() {
+
+}
+void* autoPhotoOnTagOFF() {
+
 }
