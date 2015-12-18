ea30862e935ca51451a80b99f9a05547e46f047f
Sameeh Jubran <sameeh.j@gmail.com>
2015-12-18 23:08:46 +0200

    Photos-db: Command buffers expanded Signed-off-by: Sameeh Jubran <sameeh.j@gmail.com>

--- wet.c
+++ wet.c
@@ -16,7 +16,7 @@ int main(int arc, char **argv) {
 //TODO : if there is an Error Executing the second command, should return the database into prior execution state - maybe we can assume it will work?
 void* addUser(const char* name) {
 	PGresult *res;
-	char cmd[200];
+	char cmd[800];
 	//TODO : fix the case where there is no users. what max(id) returns? - Done!
 	sprintf(cmd,
 			"insert into users values (coalesce((select max(id)+1 from users),0),\'%s\')",
@@ -44,7 +44,7 @@ void* addUser(const char* name) {
 //TODO : if there is an Error Executing the second command, should return the database into prior execution state
 void* addUserMin(const char* name) {
 	PGresult *res;
-	char cmd[200];
+	char cmd[800];
 	sprintf(cmd,
 			"insert into users values (coalesce((select min(id)+1 from users where users.id not in(select u1.id from users as u1,users as u2 where u1.id = u2.id - 1)),0),\'%s\')",
 			name);
@@ -70,7 +70,7 @@ void* addUserMin(const char* name) {
 //TODO : if the user doesn't Exist ==> print ILL_PARAMS
 void* removeUser(const char* id) {
 	PGresult *res;
-	char cmd[200];
+	char cmd[800];
 	sprintf(cmd, "select id from users where id=%s",id);
 	res = PQexec(conn, cmd);
 	if (!res || PQresultStatus(res) != PGRES_TUPLES_OK) {
@@ -99,7 +99,7 @@ void* addPhoto(const char* user_id, const char* photo_id) {
 	PGresult *res;
 	int userId = atoi(user_id);
 	int photoId = atoi(photo_id);
-	char cmd[200];
+	char cmd[800];
 	//check if user exists
 	sprintf(cmd, "select id from users where id=%d",userId);
 	res = PQexec(conn, cmd);
@@ -142,7 +142,7 @@ void* tagPhoto(const char* user_id, const char* photo_id, const char* info) {
 	PGresult *res;
 	int userId = atoi(user_id);
 	int photoId = atoi(photo_id);
-	char cmd[200];
+	char cmd[800];
 	//check if user exists
 	sprintf(cmd, "select id from users where id=%d",userId);
 	res = PQexec(conn, cmd);
@@ -183,7 +183,7 @@ void* tagPhoto(const char* user_id, const char* photo_id, const char* info) {
 }
 void* photosTags() {
 	PGresult *res;
-	char cmd[200];
+	char cmd[800];
 	sprintf(cmd,"SELECT photo_id,user_id, COUNT(photo_id) AS dupe_cnt FROM tags GROUP BY photo_id,user_id HAVING COUNT(photo_id) > 0 ORDER BY COUNT(photo_id) DESC, user_id ASC,photo_id ASC");
 	res = PQexec(conn, cmd);
 	if (!res || PQresultStatus(res) != PGRES_TUPLES_OK) {
@@ -201,7 +201,7 @@ void* photosTags() {
 }
 void* search(const char* word) {
 	PGresult *res;
-	char cmd[200];
+	char cmd[800];
 	sprintf(cmd,"SELECT photo_id,user_id, COUNT(photo_id) AS dupe_cnt FROM tags WHERE info LIKE '%%%s%%' GROUP BY photo_id,user_id HAVING COUNT(photo_id) > 0 ORDER BY COUNT(photo_id) DESC, user_id ASC,photo_id DESC",word);
 	res = PQexec(conn, cmd);
 	if (!res || PQresultStatus(res) != PGRES_TUPLES_OK) {
@@ -219,7 +219,7 @@ void* search(const char* word) {
 }
 void* commonTags(const char* k) {
 	PGresult *res;
-	char cmd[200];
+	char cmd[800];
 	sprintf(cmd,"SELECT temp.info,temp.count from(SELECT info ,(COUNT(info)) FROM tags GROUP BY info) temp where count > %d ORDER BY temp.count DESC , temp.info ASC",atoi(k));
 	res = PQexec(conn, cmd);
 	if (!res || PQresultStatus(res) != PGRES_TUPLES_OK) {
@@ -237,7 +237,7 @@ void* commonTags(const char* k) {
 }
 void* mostCommonTags(const char* k) {
 	PGresult *res;
-	char cmd[200];
+	char cmd[800];
 	sprintf(cmd,"select info,count from (select *, (select count(*) from (select * from (select info,count(info) from tags group by info) as tt order by count desc, info asc) as  p2 where p1.count < p2.count or (p1.count = p2.count and p1.info > p2.info)) as cnt from (select * from (select info,count(info) from tags group by info) as tt1 order by count desc, info asc) as p1) as pp where pp.cnt < %s",k);
 	res = PQexec(conn, cmd);
 	if (!res || PQresultStatus(res) != PGRES_TUPLES_OK) {
@@ -255,7 +255,7 @@ void* mostCommonTags(const char* k) {
 }
 void* similarPhotos(const char* k, const char* j) {
 	PGresult *res;
-	char cmd[500];
+	char cmd[800];
 	sprintf(cmd,"select user_id,n1.name,photo_id from (select user_id,photo_id,count(user_id) from (select t1.user_id,t1.photo_id,count(t1.user_id) from tags as t1,tags as t2 where t1.info = t2.info and not(t1.user_id = t2.user_id and t1.photo_id = t2.photo_id) group by t1.user_id,t2.user_id,t1.photo_id,t2.photo_id) as tagtag where tagtag.count > %s group by user_id,photo_id having count(user_id)>0) as res,users as n1 where n1.id = res.user_id and count > %s order by user_id asc,photo_id desc",j,k);
 	res = PQexec(conn, cmd);
 	if (!res || PQresultStatus(res) != PGRES_TUPLES_OK) {
@@ -273,7 +273,7 @@ void* similarPhotos(const char* k, const char* j) {
 }
 void* autoPhotoOnTagOn() {
 	PGresult *res;
-	char cmd[200];
+	char cmd[800];
 	sprintf(cmd,"CREATE OR REPLACE FUNCTION insert_missing_photo() RETURNS TRIGGER AS $$ BEGIN IF(NEW.photo_id NOT IN (SELECT photo_id FROM Tags)) THEN INSERT INTO photos VALUES(NEW.photo_id,NEW.user_id); END IF; RETURN NEW; END; $$ LANGUAGE plpgsql; CREATE TRIGGER auto_photo BEFORE INSERT ON tags FOR EACH ROW EXECUTE PROCEDURE insert_missing_photo();");
 	res = PQexec(conn, cmd);
 	if (!res || PQresultStatus(res) != PGRES_COMMAND_OK) {
@@ -285,7 +285,7 @@ void* autoPhotoOnTagOn() {
 }
 void* autoPhotoOnTagOFF() {
 	PGresult *res;
-	char cmd[200];
+	char cmd[800];
 	sprintf(cmd,"DROP TRIGGER auto_photo on tags; DROP FUNCTION insert_missing_photo();");
 	res = PQexec(conn, cmd);
 	if (!res || PQresultStatus(res) != PGRES_COMMAND_OK) {
